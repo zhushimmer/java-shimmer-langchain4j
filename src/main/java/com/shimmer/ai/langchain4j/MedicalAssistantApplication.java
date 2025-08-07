608782eb5f623ab72a74e24d5560883b2d17c731
zhushimmer <zhushimmer@gmail.com>
2025-08-07 17:12:33 +0800

    Initial commit: 添加Java项目代码

---
+++ src/main/java/com/shimmer/ai/langchain4j/MedicalAssistantApplication.java
@@ -0,0 +1,20 @@
+package com.shimmer.ai.langchain4j;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAssistantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAssistantApplication.class, args);
+
+        System.out.println("=======================Medical Assistant Application is running...==================");
+        System.out.println("=======================Medical Assistant Application is running...==================");
+        System.out.println("=======================Medical Assistant Application is running...==================");
+        System.out.println("=======================Medical Assistant Application is running...==================");
+        System.out.println("=======================Medical Assistant Application is running...==================");
+        System.out.println("=======================Medical Assistant Application is running...==================");
+
+    }
+}
